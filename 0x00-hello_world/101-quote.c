90743d0b6dd1d2536adabd9f5281f55fd5f7a857
godwin opara <godwinopara62@yahoo.com>
2022-04-07 21:45:59 +0100

    A c program that print to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print "and that piece of art is useful"
+ * Dora Korpar, 2015-10-19 followed by a newline to standard error
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	fprinf(stderr, "and that piece of art i useful\" Dora Korpar, 2015-10-19")
+}
