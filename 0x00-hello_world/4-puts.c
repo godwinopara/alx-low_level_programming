134a75f51fb9fcc19cde4584f4ce31e852ca5290
godwin opara <godwinopara62@yahoo.com>
2022-04-07 19:07:48 +0100

    A c program that prints Programming is like building a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 
-int main()
+int main(void)
 {
-	char str[] = "Programming is like building a multilingual puzzle\n"
+	char str[] = "Programming is like building a multilingual puzzle\n";
 	puts(str);
 
 	return 0;
-}
+};
+
+
 
