397f179485f86a88ef41b85792195f608eeb5913
godwin opara <godwinopara62@yahoo.com>
2022-04-07 20:08:01 +0100

    a c program that print a text

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: prints "with proper grammar, but the outcome is a piece ofart,"
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+	return (0);
 }
