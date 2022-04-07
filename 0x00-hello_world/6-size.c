3967495f836b0d8f83ad65a4e9e0d82725218049
godwin opara <godwinopara62@yahoo.com>
2022-04-07 20:35:57 +0100

    A c program that prints the size of various types on the computer it is compiled and run on.

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 /**
- *
  * main - Entry point
  *
- * Description: prints the size of various types
- * on the computer it is compiled and run on
+ * Description:prints the size of various types on the computer
+ * it is compiled
  *
  * Return: Always 0 (success)
  */
