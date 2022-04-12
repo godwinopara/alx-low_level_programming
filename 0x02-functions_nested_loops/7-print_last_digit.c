4486443fc663579f976c44827fc24ecb575eef57
godwin opara <godwinopara62@yahoo.com>
2022-04-12 16:18:20 +0100

    prints the last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print the last digit of a number
+ *
+ * @n: The number where the last digit will be printed from
+ *
+ * Return: Always 0.
+ */
+
+int print_last_digit(int n)
+{
+	int result = n / 10;
+
+	return (result);
+}
