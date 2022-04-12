77b1dd483879a024edc639aff7e14085f3c2f7d8
godwin opara <godwinopara62@yahoo.com>
2022-04-12 02:05:34 +0100

    print a-z in reverse order

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints lowercase alphabet in reverse order
+ *
+ * Return: Always (success)
+ */
+
+int main(void)
+{
+	int i = 122;
+
+	while (122 > 98)
+	{
+		putchar(i);
+		i--;
+	}
+	putchar(10);
+
+	return (0);
+}
