829e0bf3bf0d35a9f9947a42cb2c34155baf3a74
godwin opara <godwinopara62@yahoo.com>
2022-04-12 01:59:15 +0100

    print 0 - 9 using putchar function

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints 0 - 9 using putchar()
+ *
+ * Return: Always (success)
+ */
+
+int main(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		putchar(i);
+		i++;
+	}
+
+	return (0);
+}
