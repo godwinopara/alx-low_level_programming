423fdff18b8505677ee0a5609a89a1ec26964d18
godwin opara <godwinopara62@yahoo.com>
2022-04-12 16:42:41 +0100

    prints the last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,7 +11,16 @@
 
 int print_last_digit(int n)
 {
-	int result = abs(n) % 10;
+	int result;
+
+	if (n < 0)
+	{
+		result = (-1 * n) % 10;
+	}
+	else
+	{
+		result = n % 10;
+	}
 
 	_putchar(result + '0');
 	return (result);
