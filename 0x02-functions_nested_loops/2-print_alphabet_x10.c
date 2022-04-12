812ab777ff59fe6941379602c48cc35ab76db21e
godwin opara <godwinopara62@yahoo.com>
2022-04-12 15:20:26 +0100

    print a-z 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,25 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - prints 10 times the alphabet in lowercase
- * followed by newline
+ * print_az - print alphabet a - z in lowercase
  *
- * Return : Always 0.
+ * Return: Always 0.
+ */
+
+void print_az(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		_putchar(alphabet);
+	}
+	_putchar('\n');
+}
+
+/**
+ * print_alphabet_x10 - print a - z 10 times
+ *
+ * Return: Alway 0.
  */
 
 void print_alphabet_x10(void)
@@ -12,7 +28,7 @@ void print_alphabet_x10(void)
 
 	while (i < 10)
 	{
-		print_alphabet();
+		print_az();
 		i++;
 	}
 }
