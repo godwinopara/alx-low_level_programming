26cfdd825d03d72df99669c7173297dc3eeaf2e4
godwin opara <godwinopara62@yahoo.com>
2022-04-12 01:35:46 +0100

    A c program that prints the alphabet in lowercase  but ommits q and e.

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints out alphabets in lowercase without the alphabet
+ * q and e followed by a new line
+ *
+ * Return: Always (success)
+ */
+
+int main(void)
+{
+	int i = 97;
+
+	while (i < 123)
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i)	
+		}
+		i++
+	}
+
+	return (0)
+}
+
