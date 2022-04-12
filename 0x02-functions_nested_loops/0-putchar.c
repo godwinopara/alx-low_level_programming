789d99dbaaed66b5a245b97105eab32731e746b8
godwin opara <godwinopara62@yahoo.com>
2022-04-12 11:35:27 +0100

    prints the string putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints '_putchar'
+ *
+ * Return: Always (success)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
