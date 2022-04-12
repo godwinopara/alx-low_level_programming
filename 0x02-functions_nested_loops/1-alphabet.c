fc67abdb74befaf982c9ac643871a77dabdfcbbe
godwin opara <godwinopara62@yahoo.com>
2022-04-12 14:38:20 +0100

    print alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: prints alphabet in lowercase
+ * followed by newline
+ *
+ * Return: Always success.
+ */
+
+int main(void)
+{
+	char low = a;
+
+	while (low <= z)
+	{
+		_putchar(low);
+		low++;
+	}
+	_putchar("\n");
+	return (0);
+}
