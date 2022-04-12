ac821205d5ba5f0ba1d754239c346ed128cbf0a6
godwin opara <godwinopara62@yahoo.com>
2022-04-12 16:09:56 +0100

    computes absolute value to integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value to an integer
+ *
+ * @n: The number that will be computed
+ *
+ * Return: Always 0.
+ */
+
+int _abs(int n)
+{
+	return (abs(n));
+}
