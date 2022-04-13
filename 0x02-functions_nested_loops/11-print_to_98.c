48dcf400e8ec97248ff4ff0e7aafa6ce6259b574
godwin opara <godwinopara62@yahoo.com>
2022-04-13 14:27:43 +0100

    print from num to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,71 +1,22 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
- * loop_in_ascending - Loop from n (number in parameter) to 98
- * in ascending order
- *
- * @n: The number the Loop starts from
- *
- * Return: Always success.
+ * print_to_98 - print all natural numbers from n to 98.
+ * @n: the number to start counting from n to 98
+ * Return: Always 0.
  */
-
-void loop_in_ascending(int n)
-{
-	for (; n <= 98; n++)
-	{
-		if (n == 98)
-		{
-			printf("%d", n);
-		}
-		else
-		{
-			printf("%d, ", n);
-		}
-	}
-}
-
-/**
- * loop_in_decending - Loop from 98 down to (n) number in parameter
- * in decending order
- *
- * @n: The number the Loop starts from
- *
- * Return: Always success.
- */
-
-void loop_in_decending(int n)
-{
-	for (; n >= 98; n--)
-	{
-		if (n == 98)
-		{
-			printf("%d ", n);
-		}
-		else
-		{
-			printf("%d, ", n);
-		}
-	}
-	printf("\n");
-}
-/**
- * print_to_98 - print from n (number in parameter) to 98
- *
- * @n: The number to start printing from
- *
- * Return: Always success.
- *
- */
-
 void print_to_98(int n)
 {
-	if (n < 98)
-	{
-		loop_in_ascending(n);
-	}
-	else
-	{
-		loop_in_decending(n);
-	}
+if (n < 98)
+{
+for (n = n; n < 98; n++)
+printf("%d, ", n);
+printf("%d\n", 98);
+}
+else
+{
+for (n = n; n > 98; n--)
+printf("%d, ", n);
+printf("%d\n", 98);
+}
 }
