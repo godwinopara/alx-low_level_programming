b98c77def3af56e599bce89a48a3860a3cb78e2b
godwin opara <godwinopara62@yahoo.com>
2022-04-13 12:11:48 +0100

    print the 9 times table starting with 0

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * times_table - print the 9 times tables starting with 0
+ *
+ * Return: Always 0.
+ */
+
+void times_table(void)
+{
+	int i;
+
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (j == 9)
+			{
+				_putchar(i);
+			}
+			else
+			{
+				_putchar(i + ', ');
+			}
+		}
+	}
+}
