285054fa71dc3a592deb4a86f3db9aaf288a10d7
godwin opara <godwinopara62@yahoo.com>
2022-04-13 13:22:35 +0100

    print the 9 times table starting with 0

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,39 +1,47 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * times_table - print the 9 times tables starting with 0
+ * times_table -prints tables
  *
  * Return: Always 0.
  */
-
 void times_table(void)
 {
-	int i;
-
-	int j;
-
-	int k;
-
-	for (i = 0; i <= 9; i++)
-	{
-		for (j = 0; j <= 9; j++)
-		{
-			k = i * j;
-
-			if (j == 9)
-			{
-				printf("%2d", k);
-			}
-			else if (k > 9)
-			{
-				printf("%1d, ", k);
-			}
-			else
-			{
-				printf("%2d, ", k);
-			}
-		}
-		printf("\n");
-	}
+int a;
+int b;
+int c;
+for (a = 0; a <= 9; a++)
+{
+for (b = 0; b <= 9; b++)
+{
+c = a * b;
+if ((c / 10) == 0)
+{
+if (b == 0)
+{
+_putchar ('0');
+}
+if (b != 0)
+{
+_putchar (' ');
+_putchar ((c % 10) + '0');
+}
+if (b < 9)
+{
+_putchar(',');
+_putchar (' ');
+}
+}
+else
+{
+_putchar ((c / 10) + '0');
+_putchar ((c % 10) + '0');
+if (b < 9)
+{
+_putchar(',');
+_putchar (' ');
+}
+}
+}
+_putchar ('\n');
+}
 }
