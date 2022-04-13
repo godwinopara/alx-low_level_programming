c761387e0022d8967e67e8795c08e21f4fa2a640
godwin opara <godwinopara62@yahoo.com>
2022-04-13 13:32:25 +0100

    Returns the sum of two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - print the sum of two numbers a and b
+ *
+ * @a: the first number
+ * @b: the second number
+ *
+ *
+ * Return: Alway success.
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
