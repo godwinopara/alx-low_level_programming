6372c5f8449156d369a15587168cab63fe4b132b
godwin opara <godwinopara62@yahoo.com>
2022-04-13 15:43:00 +0100

    print the sum of all the multiples of 3 and 5

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,7 +7,7 @@
  * Description: Prints the sum of all the multiples of 3 or 5
  * below 1024
  *
- * Return Always 0.
+ * Return: Always 0.
  */
 
 int main(void)
@@ -27,7 +27,7 @@ int main(void)
 			sum += i;
 		}
 	}
-
+	printf("%d", sum);
 	printf("\n");
 	return (0);
 }
