2c33bbe8deeee25d4b27354bffeb5e25ee64465e
godwin opara <godwinopara62@yahoo.com>
2022-04-14 16:26:48 +0100

    print 0 - 14 from ascii table

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,78 +1,23 @@
 #include "main.h"
 
 /**
- * print_0_to_9 - print numbers 0 - 9
- *
- * Return: Always success.
+ * more_numbers - prints 10 times the numbers, from 0 to 14
  */
-void print_0_to_9(void);
-
-/**
- * print_10_to_14 - print numbers 10 - 14
- *
- * Return: Always success.
- */
-
-void print_10_to_14(void);
-
-/**
- * more_numbers - print numbers 0 - 14
- * in 10 places
- *
- *
- * Return: Always success.
- *
- */
-
 
 void more_numbers(void)
 {
-	int i = 0;
+	int c, x;
 
-	while (i < 10)
+	for (c = 0; c < 10; c++)
 	{
-		print_0_to_9();
-		print_10_to_14();
+		for (x = 0; x <= 14; x++)
+		{
+			if (x > 9)
+			{
+				_putchar((x / 10) + 48);
+			}
+			_putchar((x % 10) + 48);
+		}
 		_putchar(10);
-		i++;
 	}
 }
-
-/**
- * print_0_to_9 - print 0 - 9
- *
- * Description: print the number 0 - 9
- *
- * Return: Always success
- */
-
-void print_0_to_9(void)
-{
-	int i = 48;
-
-	while (i <= 57)
-	{
-		_putchar(i);
-		i++;
-	}
-}
-
-
-/**
- * print_10_to_14 - print number 10 - 14 from ascii
- *
- * Return: Always success
- */
-
-void print_10_to_14(void)
-{
-	int i = 48;
-
-	while (i <= 52)
-	{
-		_putchar(49);
-		_putchar(i);
-		i++;
-	}
-}
-
