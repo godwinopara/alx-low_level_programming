e86a03e360f24786c637c3e9edae48a66eb60a1a
godwin opara <godwinopara62@yahoo.com>
2022-04-15 00:59:16 +0100

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,7 +13,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 100; i++)
+	for (i = 1; i < 100; i++)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
 		{
@@ -25,7 +25,7 @@ int main(void)
 		}
 		else if (i % 5 == 0)
 		{
-			prinf("Buzz");
+			printf("Buzz");
 		}
 		else
 		{
