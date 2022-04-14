c4178aff8b1b5bc067c3b71ea7cea657fa0f7624
godwin opara <godwinopara62@yahoo.com>
2022-04-14 14:46:46 +0100

    return the result of the product of two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * mul - print the result of the multiplication
+ * of two number
+ *
+ * @a: first integer
+ * @b:  second integer
+ *
+ * Return: Always 0.
+ *
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	return (result);
+}
