e875196e93f8e49d6d9f54fd3bd7aa9c439d3180
godwin opara <godwinopara62@yahoo.com>
2022-04-14 16:40:19 +0100

    print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - print "-" on stdout
+ *
+ * @n: the number of "-" to print
+ *
+ * Return: Always success.
+ */
+
+void print_line(int n)
+{
+	int i = 0;
+
+
+	if (n > 0)
+	{
+		while (i < n)
+		{
+			_putchar('\\');
+			i++;
+		}
+	}
+
+	_putchar(10);
+}
