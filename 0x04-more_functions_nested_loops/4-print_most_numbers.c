b0e181592020e24374b4ca0393ec529d82a65f69
godwin opara <godwinopara62@yahoo.com>
2022-04-14 15:33:45 +0100

    print ascii number 0 - 9 omitting numbers 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print numbers from 0 - 9
+ * omitting 2 and 4
+ *
+ * Return: Always success.
+ *
+ */
+
+void print_most_numbers(void);
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		if (i != 50 || i != 52)
+		{
+			_putchar(i);
+		}
+	}
+
+	_putchar('\n');
+}
