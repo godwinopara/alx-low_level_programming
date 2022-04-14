7e4af813a4ebff76ddc0b805fb644d6d953b8390
godwin opara <godwinopara62@yahoo.com>
2022-04-14 16:34:06 +0100

    print lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.c"
+
+/**
+ * print_line - print "-" on stdout
+ *
+ * @n: the number of "-" to print
+ *
+ * Return: Always success.
+ */
+
+void print_line(int n);
+{
+	int i = 0;
+
+
+	if (n > 0)
+	{
+		while (i < n)
+		{
+			_putchar('_');
+			i++;
+		}
+	}
+
+	_putchar(10);
+}
