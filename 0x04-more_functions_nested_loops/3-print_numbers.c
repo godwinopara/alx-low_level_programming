b675676c59a58dccd589c9e016557738c203267e
godwin opara <godwinopara62@yahoo.com>
2022-04-14 15:02:56 +0100

    print 0 - 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print number 0 - 9
+ *
+ * Return: Always success.
+ *
+ */
+
+void print_numbers(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar("\n")
+}
