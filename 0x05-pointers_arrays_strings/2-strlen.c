8d6028ad9db0dbb2a76ad02750904625544f774d
godwin opara <godwinopara62@yahoo.com>
2022-04-26 22:31:32 +0100

    return the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - Return the length of a string.
+ *
+ * @s: the string that its when will be checked
+ *
+ * Return: Always success
+ *
+ */
+
+int _strlen(char *s)
+{
+	int length = strlen(s);
+
+	return (length);
+}
+
