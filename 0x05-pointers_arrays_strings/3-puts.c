9c6d446dd06ebbae2fb31900367d0c90ca1bb579
godwin opara <godwinopara62@yahoo.com>
2022-04-26 22:49:43 +0100

    print a string followed by a new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _puts - print a string followed by a newline
+ *
+ * @str: the string that will be printed
+ *
+ * Return: Always success
+ *
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+	puts(10);
+}
