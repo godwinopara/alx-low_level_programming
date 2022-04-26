d1d4ce289e739dfad87bf23a0578aaebb7de6429
godwin opara <godwinopara62@yahoo.com>
2022-04-26 22:05:47 +0100

    reset the parameter and update the value to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: updates the value it points to to 98.
+ *
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+	_putchar(n);
+}
+
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+0x05 pointers array and strings
